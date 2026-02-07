25c79e0cb278ca4d6ca89f6e4966d0e47a288960
Mghiremth0281 <muttuh028@gmail.com>
2026-02-07 14:39:33 +0530

    Done with the basic Setup

---
+++ src/main/java/gritgear/example/GritGear/GritGearApplication.java
@@ -0,0 +1,13 @@
+package gritgear.example.GritGear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GritGearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GritGearApplication.class, args);
+	}
+
+}
