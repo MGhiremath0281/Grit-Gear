8d7ac1eae2617947b8a15ade4ef59d229f504cab
Mghiremth0281 <muttuh028@gmail.com>
2026-02-08 20:45:53 +0530

    created User DTo

---
+++ src/main/java/gritgear/example/GritGear/dto/UserRequestDTO.java
@@ -0,0 +1,50 @@
+package gritgear.example.GritGear.dto;
+
+public class UserRequestDTO {
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+    private String phoneNumber;
+
+     public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+}
