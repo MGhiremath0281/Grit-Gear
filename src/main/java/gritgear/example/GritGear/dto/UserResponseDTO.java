2c18783e0701ba8291ceb89bba9e177731e7b7f2
Mghiremth0281 <muttuh028@gmail.com>
2026-02-09 18:24:14 +0530

    Added the userResponse DTO

---
+++ src/main/java/gritgear/example/GritGear/dto/UserResponseDTO.java
@@ -0,0 +1,43 @@
+package gritgear.example.GritGear.dto;
+
+public class UserResponseDTO {
+    private Long id;
+    private String fullName;
+    private String email;
+    private String role;
+    private String phoneNumber;
+
+      public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    
+}
