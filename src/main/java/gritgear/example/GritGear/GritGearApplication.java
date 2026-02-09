173a9add3d9f5f3bc00e5046361f1ef19daffb0b
Mghiremth0281 <muttuh028@gmail.com>
2026-02-09 19:24:48 +0530

    Added the MP configuration in teh config Package

--- src/main/java/gritgear/example/GritGear/GritGearApplication.java
+++ src/main/java/gritgear/example/GritGear/GritGearApplication.java
@@ -1,7 +1,9 @@
 package gritgear.example.GritGear;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GritGearApplication {
