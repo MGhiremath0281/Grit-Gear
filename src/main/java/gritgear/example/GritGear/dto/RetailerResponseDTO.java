24d7eaa0d4fa6db8510cdac883b1a5adb9394b95
Mghiremth0281 <muttuh028@gmail.com>
2026-02-12 19:23:30 +0530

    addte dteh email object in the response file

--- src/main/java/gritgear/example/GritGear/dto/RetailerResponseDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/RetailerResponseDTO.java
@@ -4,6 +4,15 @@ public class RetailerResponseDTO {
     private Long id;
     private String name;
     private String phone;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public Long getId() {
         return id;
