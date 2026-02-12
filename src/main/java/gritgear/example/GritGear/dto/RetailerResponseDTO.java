efc56c58ce7be3ece1d0d794d644bc7a763bb47b
Mghiremth0281 <muttuh028@gmail.com>
2026-02-12 18:54:19 +0530

    Added the retailer Ressponse DTO

--- src/main/java/gritgear/example/GritGear/dto/RetailerResponseDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/RetailerResponseDTO.java
@@ -1,4 +1,50 @@
 package gritgear.example.GritGear.dto;
 
 public class RetailerResponseDTO {
+    private Long id;
+    private String name;
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProducts() {
+        return products;
+    }
+
+    public void setProducts(String products) {
+        this.products = products;
+    }
+
+    private String address;
+    private String products;
 }
