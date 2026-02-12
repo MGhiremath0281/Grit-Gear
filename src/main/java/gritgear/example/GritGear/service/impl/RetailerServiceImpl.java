456fca25efc2c4315b3e47151a2cb04c60069e74
Mghiremth0281 <muttuh028@gmail.com>
2026-02-12 19:34:15 +0530

    Changed the method for the delete retailer

--- src/main/java/gritgear/example/GritGear/service/impl/RetailerServiceImpl.java
+++ src/main/java/gritgear/example/GritGear/service/impl/RetailerServiceImpl.java
@@ -1,11 +1,14 @@
 package gritgear.example.GritGear.service.impl;
 
+import gritgear.example.GritGear.dto.RetailerRequestDTO;
+import gritgear.example.GritGear.dto.RetailerResponseDTO;
 import gritgear.example.GritGear.model.Retailer;
 import gritgear.example.GritGear.repositry.RetailerRepositry;
 import gritgear.example.GritGear.service.RetailerService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RetailerServiceImpl implements RetailerService {
@@ -17,43 +20,64 @@ public class RetailerServiceImpl implements RetailerService {
     }
 
     @Override
-    public Retailer createRetailer(Retailer retailer) {
-        return retailerRepositry.save(retailer);
+    public RetailerResponseDTO createRetailer(RetailerRequestDTO dto) {
+            Retailer retailer = new Retailer();
+            retailer.setName(dto.getName());
+            retailer.setPassword(dto.getPassword());
+            retailer.setPhone(dto.getPhone());
+            retailer.setAddress(dto.getAddress());
+            retailer.setProducts(dto.getProducts());
+            Retailer saved = retailerRepositry.save(retailer);
+            return  mapToResponseDTO(saved);
+
     }
 
     @Override
-    public Retailer getRetailerById(Long id) {
-        return retailerRepositry.findById(id).orElse(null);
+    public RetailerResponseDTO getRetailerById(Long id) {
+        return retailerRepositry.findById(id)
+                .map(this::mapToResponseDTO)
+                .orElse(null);
     }
 
     @Override
-    public List<Retailer> getAllRetailers() {
-        return retailerRepositry.findAll();
+    public List<RetailerResponseDTO> getAllRetailers() {
+        return retailerRepositry.findAll()
+                .stream()
+                .map(this::mapToResponseDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Retailer updateRetailer(Long id, Retailer retailer) {
+    public RetailerResponseDTO updateRetailer(Long id, RetailerRequestDTO dto) {
 
         Retailer existing = retailerRepositry.findById(id).orElse(null);
         if (existing == null) return null;
 
-        existing.setName(retailer.getName());
-        existing.setEmail(retailer.getEmail());
-        existing.setAddress(retailer.getAddress());
-        existing.setPhone(retailer.getPhone());
-        existing.setProducts(retailer.getProducts());
+        existing.setName(dto.getName());
+        existing.setEmail(dto.getEmail());
+        existing.setAddress(dto.getAddress());
+        existing.setPhone(dto.getPhone());
+        existing.setProducts(dto.getProducts());
 
-        return retailerRepositry.save(existing);
+       Retailer updated = retailerRepositry.save(existing);
+       return mapToResponseDTO(updated);
     }
 
     @Override
-    public Retailer deleteRetailer(Long id) {
+    public void deleteRetailer(Long id) {
 
-        Retailer existing = retailerRepositry.findById(id).orElse(null);
-        if (existing == null) return null;
+    }
 
-        retailerRepositry.delete(existing);
-        return existing;
+
+    private RetailerResponseDTO mapToResponseDTO(Retailer retailer){
+        RetailerResponseDTO response = new RetailerResponseDTO();
+        response.setId(retailer.getId());
+        response.setName(retailer.getName());
+        response.setAddress(retailer.getAddress());
+        response.setEmail(retailer.getEmail());
+        response.setPhone(retailer.getPhone());
+        response.setProducts(retailer.getProducts();
+        return response;
     }
 }
 
