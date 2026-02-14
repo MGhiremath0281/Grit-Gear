dd8e7237d21b42281573f83cb12f2d409646c126
Mghiremth0281 <muttuh028@gmail.com>
2026-02-14 23:03:18 +0530

    Added the ccontent to the both teh dto files

--- src/main/java/gritgear/example/GritGear/dto/ProductrequestDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/ProductrequestDTO.java
@@ -1,4 +1,76 @@
 package gritgear.example.GritGear.dto;
 
 public class ProductrequestDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private String category;
+    private String imageUrl;
+    private String retailer;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getRetailer() {
+        return retailer;
+    }
+
+    public void setRetailer(String retailer) {
+        this.retailer = retailer;
+    }
 }
