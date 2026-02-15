a188d8307a34ed63f31019e999b9fc310164288f
Mghiremth0281 <muttuh028@gmail.com>
2026-02-15 13:04:14 +0530

    Added the entity and repositry

---
+++ src/main/java/gritgear/example/GritGear/model/OrderItem.java
@@ -0,0 +1,48 @@
+package gritgear.example.GritGear.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String produts;
+    private Integer quantity;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduts() {
+        return produts;
+    }
+
+    public void setProduts(String produts) {
+        this.produts = produts;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
