0c5aafdf70eb98628e0bc33683c6fa65529e5cb1
Mghiremth0281 <muttuh028@gmail.com>
2026-02-16 19:29:27 +0530

    Added teh controller for the order Entity

---
+++ src/main/java/gritgear/example/GritGear/controller/OrderController.java
@@ -0,0 +1,45 @@
+package gritgear.example.GritGear.controller;
+
+import gritgear.example.GritGear.dto.OrderRequestDTO;
+import gritgear.example.GritGear.dto.OrderResponseDTO;
+import gritgear.example.GritGear.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public OrderResponseDTO createOrder(@RequestBody OrderRequestDTO dto) {
+        return orderService.createOrder(dto);
+    }
+
+    @GetMapping
+    public List<OrderResponseDTO> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponseDTO getOrderById(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
+
+    @PutMapping("/{id}")
+    public OrderResponseDTO updateOrder(@PathVariable Long id,
+                                        @RequestBody OrderRequestDTO dto) {
+        return orderService.updateOrder(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOrder(@PathVariable Long id) {
+        orderService.deleteOrder(id);
+    }
+}
