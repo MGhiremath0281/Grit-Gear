217df2aa9e11bbccd3d67e7b6a810a053eba13f0
Mghiremth0281 <muttuh028@gmail.com>
2026-02-16 19:36:53 +0530

    created the the Cartitem entity

---
+++ src/main/java/gritgear/example/GritGear/model/CartItem.java
@@ -0,0 +1,40 @@
+package gritgear.example.GritGear.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String product;
+    private Integer quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
