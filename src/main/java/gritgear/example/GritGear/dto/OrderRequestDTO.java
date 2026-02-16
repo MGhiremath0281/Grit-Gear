efa331c9e8e6247c52d8f4d107cbfe99cf395bcf
Mghiremth0281 <muttuh028@gmail.com>
2026-02-16 19:16:53 +0530

    Added the both Request An dthe Response DTO

---
+++ src/main/java/gritgear/example/GritGear/dto/OrderRequestDTO.java
@@ -0,0 +1,37 @@
+package gritgear.example.GritGear.dto;
+
+import java.math.BigDecimal;
+
+public class OrderRequestDTO {
+
+    private Long userId;
+    private String orderItems;
+    private BigDecimal totalAmount;
+
+    public OrderRequestDTO() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(String orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
---
+++ src/main/java/gritgear/example/GritGear/dto/OrderResponseDTO.java
@@ -0,0 +1,66 @@
+package gritgear.example.GritGear.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderResponseDTO {
+
+    private Long id;
+    private Long userId;
+    private String orderItems;
+    private BigDecimal totalAmount;
+    private String status;
+    private LocalDateTime createdAt;
+
+    public OrderResponseDTO() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(String orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    public BigDecimal getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(BigDecimal totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
