0860c173eba207fa631ea3c85624d19e785924ab
Mghiremth0281 <muttuh028@gmail.com>
2026-02-16 19:21:36 +0530

    Added the Service file

---
+++ src/main/java/gritgear/example/GritGear/service/OrderService.java
@@ -0,0 +1,19 @@
+package gritgear.example.GritGear.service;
+
+import gritgear.example.GritGear.dto.OrderRequestDTO;
+import gritgear.example.GritGear.dto.OrderResponseDTO;
+
+import java.util.List;
+
+public interface OrderService {
+
+    OrderResponseDTO createOrder(OrderRequestDTO dto);
+
+    List<OrderResponseDTO> getAllOrders();
+
+    OrderResponseDTO getOrderById(Long id);
+
+    OrderResponseDTO updateOrder(Long id, OrderRequestDTO dto);
+
+    void deleteOrder(Long id);
+}
