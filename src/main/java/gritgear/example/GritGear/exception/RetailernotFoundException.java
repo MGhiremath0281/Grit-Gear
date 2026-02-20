debecee633620f8abbf1a311ae904bf9f838208e
Mghiremth0281 <muttuh028@gmail.com>
2026-02-20 16:58:20 +0530

    Created retailer Not found Exception

--- src/main/java/gritgear/example/GritGear/exception/RetailernotFoundException.java
+++ src/main/java/gritgear/example/GritGear/exception/RetailernotFoundException.java
@@ -1,4 +1,8 @@
 package gritgear.example.GritGear.exception;
 
-public class RetailernotFoundException {
+public class RetailernotFoundException extends RuntimeException{
+
+    public RetailernotFoundException(String message){
+        super(message);
+    }
 }
