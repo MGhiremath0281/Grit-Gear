a44f38572839150b5c52378d5bf82b4129a6731a
Mghiremth0281 <muttuh028@gmail.com>
2026-02-28 22:50:26 +0530

    feat: implement request validation in Retailer

--- src/main/java/gritgear/example/GritGear/dto/retailer/RetailerRequestDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/retailer/RetailerRequestDTO.java
@@ -1,10 +1,29 @@
 package gritgear.example.GritGear.dto.retailer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class RetailerRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     private String phone;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
+
+    @NotBlank(message = "Address is required")
+    @Size(min = 5, max = 255, message = "Address must be between 5 and 255 characters")
     private String address;
 
     public String getPassword() {
