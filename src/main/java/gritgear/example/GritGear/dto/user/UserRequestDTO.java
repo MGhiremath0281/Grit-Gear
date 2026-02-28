c5b6747815219299b35dd5e7a5b5ce54b7141e40
Mghiremth0281 <muttuh028@gmail.com>
2026-02-28 22:48:49 +0530

    feat: implement request validation in user

--- src/main/java/gritgear/example/GritGear/dto/user/UserRequestDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/user/UserRequestDTO.java
@@ -1,13 +1,33 @@
 package gritgear.example.GritGear.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class UserRequestDTO {
+
+
+    @NotBlank(message = "Full name is required")
+    @Size(min = 3, max = 50, message = "Full name must be between 3 and 50 characters")
     private String fullName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
+    @NotBlank(message = "Role is required")
     private String role;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone number must be 10 digits")
     private String phoneNumber;
 
-     public String getFullName() {
+    public String getFullName() {
         return fullName;
     }
 
