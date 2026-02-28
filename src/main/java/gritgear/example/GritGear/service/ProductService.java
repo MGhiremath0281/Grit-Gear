57ea9c5719ad45a85f56160e7a9d32e917c04e52
Mghiremth0281 <muttuh028@gmail.com>
2026-02-28 22:16:12 +0530

    feat: add pagination support to ProductService

--- src/main/java/gritgear/example/GritGear/service/ProductService.java
+++ src/main/java/gritgear/example/GritGear/service/ProductService.java
@@ -2,14 +2,13 @@ package gritgear.example.GritGear.service;
 
 import gritgear.example.GritGear.dto.product.ProductResponseDTO;
 import gritgear.example.GritGear.dto.product.ProductRequestDTO;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface ProductService {
 
     ProductResponseDTO createProduct(ProductRequestDTO dto);
 
-    List<ProductResponseDTO> getAllProducts();
+    Page<ProductResponseDTO> getAllProducts(int page, int size);
 
     ProductResponseDTO getProductById(Long id);
 
