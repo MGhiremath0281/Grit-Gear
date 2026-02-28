7555d117458afeb8508f9dfc395f58effd722e97
Mghiremth0281 <muttuh028@gmail.com>
2026-02-28 22:57:52 +0530

    feat(Category): add request validation

--- src/main/java/gritgear/example/GritGear/dto/category/CategoryRequestDTO.java
+++ src/main/java/gritgear/example/GritGear/dto/category/CategoryRequestDTO.java
@@ -1,7 +1,15 @@
 package gritgear.example.GritGear.dto.category;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CategoryRequestDTO {
+    @NotBlank(message = "Product name is required")
+    @Size(min = 3, max = 100, message = "Product name must be between 3 and 100 characters")
     private String proname;
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 5, max = 300, message = "Description must be between 5 and 300 characters")
     private String description;
 
     public String getProname() {
