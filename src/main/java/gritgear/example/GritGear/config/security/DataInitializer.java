1f4fc2bd21e4a33afb08361337ea6677ff973cd7
Mghiremth0281 <muttuh028@gmail.com>
2026-03-01 23:02:08 +0530

    chore(security): add admin credentials to database

--- src/main/java/gritgear/example/GritGear/config/security/DataInitializer.java
+++ src/main/java/gritgear/example/GritGear/config/security/DataInitializer.java
@@ -1,4 +1,46 @@
 package gritgear.example.GritGear.config.security;
 
+import gritgear.example.GritGear.model.Role;
+import gritgear.example.GritGear.model.User;
+import gritgear.example.GritGear.repositry.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DataInitializer {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    public DataInitializer(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostConstruct
+    public void createAdmin() {
+
+        if (userRepository.findByEmail(adminEmail).isEmpty()) {
+
+            User admin = new User();
+            admin.setFullName("Super Admin");
+            admin.setEmail(adminEmail);
+            admin.setPassword(passwordEncoder.encode(adminPassword));
+            admin.setRole(Role.ROLE_ADMIN);
+            admin.setActive(true);
+
+            userRepository.save(admin);
+
+            System.out.println("Admin user created successfully.");
+        }
+    }
 }
