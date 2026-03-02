37b2952ab41e437992561964b6ca26b6a2568734
Mghiremth0281 <muttuh028@gmail.com>
2026-03-02 17:27:19 +0530

    feat(security): add RBAC for product

--- src/main/java/gritgear/example/GritGear/controller/ProductController.java
+++ src/main/java/gritgear/example/GritGear/controller/ProductController.java
@@ -4,13 +4,11 @@ import gritgear.example.GritGear.dto.product.ProductResponseDTO;
 import gritgear.example.GritGear.dto.product.ProductRequestDTO;
 import gritgear.example.GritGear.service.ProductService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -23,43 +21,41 @@ public class ProductController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('RETAILER', 'ADMIN')")
     public ResponseEntity<ProductResponseDTO> createProduct(
             @Valid @RequestBody ProductRequestDTO dto) {
-
         ProductResponseDTO saved = productService.createProduct(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(saved);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductResponseDTO> getProductById(
-            @PathVariable Long id) {
-
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
         ProductResponseDTO product = productService.getProductById(id);
         return ResponseEntity.ok(product);
     }
 
     @GetMapping
+    @PreAuthorize("permitAll()")
     public ResponseEntity<Page<ProductResponseDTO>> getAllProducts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
-
         return ResponseEntity.ok(productService.getAllProducts(page, size));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or (hasRole('RETAILER') and @productSecurity.isProductOwner(#id))")
     public ResponseEntity<ProductResponseDTO> updateProduct(
             @PathVariable Long id,
             @Valid @RequestBody ProductRequestDTO dto) {
-
         ProductResponseDTO updated = productService.updateProduct(id, dto);
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(
-            @PathVariable Long id) {
-
+    @PreAuthorize("hasRole('ADMIN') or (hasRole('RETAILER') and @productSecurity.isProductOwner(#id))")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
