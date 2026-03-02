876a582194924412c6f9b423174b2310c8324d92
Mghiremth0281 <muttuh028@gmail.com>
2026-03-02 17:03:21 +0530

    fix(security): solved teh retailer entity issue

--- src/main/java/gritgear/example/GritGear/controller/AuthController.java
+++ src/main/java/gritgear/example/GritGear/controller/AuthController.java
@@ -2,9 +2,11 @@ package gritgear.example.GritGear.controller;
 
 import gritgear.example.GritGear.config.security.JwtUtil;
 import gritgear.example.GritGear.dto.auth.AuthResponse;
+import gritgear.example.GritGear.dto.retailer.RetailerRequestDTO;
 import gritgear.example.GritGear.dto.user.UserRequestDTO;
 import gritgear.example.GritGear.dto.user.UserResponseDTO;
 import gritgear.example.GritGear.model.Role;
+import gritgear.example.GritGear.service.RetailerService;
 import gritgear.example.GritGear.service.UserService;
 
 import org.springframework.http.ResponseEntity;
@@ -21,19 +23,21 @@ public class AuthController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final RetailerService retailerService;
 
     public AuthController(UserService userService,
                           AuthenticationManager authenticationManager,
-                          JwtUtil jwtUtil) {
+                          JwtUtil jwtUtil, RetailerService retailerService) {
         this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
+        this.retailerService = retailerService;
     }
 
     @PostMapping("/register/user")
     public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO dto) {
 
-        dto.setRole(Role.ROLE_USER);  // Force role
+        dto.setRole(Role.ROLE_USER);
         UserResponseDTO savedUser = userService.createUser(dto);
 
         return ResponseEntity.ok(savedUser);
@@ -42,10 +46,17 @@ public class AuthController {
     @PostMapping("/register/retailer")
     public ResponseEntity<UserResponseDTO> registerRetailer(@RequestBody UserRequestDTO dto) {
 
-        dto.setRole(Role.ROLE_RETAILER);  // Force role
-        UserResponseDTO savedRetailer = userService.createUser(dto);
+        dto.setRole(Role.ROLE_RETAILER);
+        UserResponseDTO savedUser = userService.createUser(dto);
 
-        return ResponseEntity.ok(savedRetailer);
+        RetailerRequestDTO retailerDto = new RetailerRequestDTO();
+        retailerDto.setEmail(dto.getEmail());
+        retailerDto.setName(dto.getFullName());
+        retailerDto.setPassword(dto.getPassword());
+
+        retailerService.createRetailer(retailerDto);
+
+        return ResponseEntity.ok(savedUser);
     }
 
     @PostMapping("/login")
